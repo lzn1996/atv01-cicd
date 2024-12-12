1d06fe157e0a49e416deb7a2a29962afa67edf3a
lzn1996 <luan6992@outlook.com>
2024-12-11 21:24:09 -0300

    add teste pra nao passar

--- test_password_generator.py
+++ test_password_generator.py
@@ -4,10 +4,8 @@ from password_generator import generate_password
 class TestPasswordGenerator(unittest.TestCase):
     def test_password_length(self):
         password = generate_password(10)
-        self.assertEqual(len(password), 10)
+        self.assertEqual(len(password), 8)
 
     def test_password_contains_uppercase(self):
         password = generate_password(10, use_uppercase=True)
         self.assertRegex(password, '[A-Z]')
-
-    # Adicione mais testes para verificar outros critérios, como a presença de letras minúsculas, números e caracteres especiais.
