e3f397c262de26589639a84736f27847296f4608
lzn1996 <luan6992@outlook.com>
2024-12-11 21:27:06 -0300

    add teste pra passar e com projeto compactado

--- test_password_generator.py
+++ test_password_generator.py
@@ -4,7 +4,7 @@ from password_generator import generate_password
 class TestPasswordGenerator(unittest.TestCase):
     def test_password_length(self):
         password = generate_password(10)
-        self.assertEqual(len(password), 8)
+        self.assertEqual(len(password), 10)
 
     def test_password_contains_uppercase(self):
         password = generate_password(10, use_uppercase=True)
